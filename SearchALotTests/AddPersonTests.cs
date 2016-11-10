bff66f849f9211493347331ac6cf31026d15235c
quickhorn <quickhorn@gmail.com>
2016-11-10 10:38:51 -0700

    More unit tests

--- SearchALotTests/AddPersonTests.cs
+++ SearchALotTests/AddPersonTests.cs
@@ -25,7 +25,7 @@ namespace SearchALotTests
             mUnitOfWork.Setup(f => f.Persons.AddPerson(It.IsAny<Person>()));
 
             var personManagementService = new PersonManagementService(mUnitOfWork.Object);
-            var personDto = new PersonDTO() { FirstName = "Myname", LastName = "Friend" };
+            var personDto = new PersonDTO() { FirstName = "Myname", LastName = "Friend", City = "Cityssss", ImageUrl = "image.gif", State = "UT", StreetAddress = "MyAddress", ZipCode = "84444" };
 
 
             //action
@@ -68,6 +68,46 @@ namespace SearchALotTests
             var response = personManagementService.AddPerson(personDto);
 
             //assert
+            Assert.IsFalse(response.Success);
+        }
+
+
+        [TestMethod]
+        public void TestMissingAddress()
+        {
+            var mUnitOfWork = new Mock<ISearchALotUnitOfWork>();
+            mUnitOfWork.Setup(f => f.Persons.AddPerson(It.IsAny<Person>()));
+
+            var personManagementService = new PersonManagementService(mUnitOfWork.Object);
+
+            var personDtoMissingCity = new PersonDTO() { FirstName = "MyName", LastName = "LastName", State = "UT", StreetAddress = "45", ZipCode = "45345", ImageUrl = "image.gif" };
+            var response = personManagementService.AddPerson(personDtoMissingCity);
+            Assert.IsFalse(response.Success);
+
+            var personDtoMissingState = new PersonDTO() { FirstName = "MyName", LastName = "LastName", StreetAddress = "45", City = "Cityzz", ZipCode = "45345", ImageUrl = "image.gif" };
+            response = personManagementService.AddPerson(personDtoMissingState);
+            Assert.IsFalse(response.Success);
+
+            var personDtoMissingStreetAddress = new PersonDTO() { FirstName = "MyName", LastName = "LastName", State = "UT", City = "Cityzz", ZipCode = "45345", ImageUrl = "image.gif" };
+            response = personManagementService.AddPerson(personDtoMissingStreetAddress);
+            Assert.IsFalse(response.Success);
+
+            var personDtoMissingZip = new PersonDTO() { FirstName = "Myname", LastName = "Friend", City = "Cityssss", ImageUrl = "image.gif", State = "UT", StreetAddress = "MyAddress" };
+            response = personManagementService.AddPerson(personDtoMissingZip);
+            Assert.IsFalse(response.Success);
+        }
+
+
+        [TestMethod]
+        public void TestMissingImageUrl()
+        {
+            var mUnitOfWork = new Mock<ISearchALotUnitOfWork>();
+            mUnitOfWork.Setup(f => f.Persons.AddPerson(It.IsAny<Person>()));
+
+            var personManagementService = new PersonManagementService(mUnitOfWork.Object);
+
+            var personDto = new PersonDTO() { FirstName = "Myname", LastName = "Friend", City = "Cityssss", State = "UT", StreetAddress = "MyAddress", ZipCode = "84444" };
+            var response = personManagementService.AddPerson(personDto);
             Assert.IsFalse(response.Success);
         }
     }
--- SirSearchALotBusinessLogic/IntegratedServiceImplementations/PersonManagementService.cs
+++ SirSearchALotBusinessLogic/IntegratedServiceImplementations/PersonManagementService.cs
@@ -24,13 +24,22 @@ namespace SirSearchALotBusinessLogic.IntegratedImplementations
         {
             try
             {
+                //This validation could be better in the DTO and using ModelState.
                 if (string.IsNullOrEmpty(person.FirstName) || string.IsNullOrEmpty(person.LastName))
                 {
                     return new AddPersonResponse() { Message = "Missing Argument. First and last name must be included for a person. For single named persons, use that name twice (E.g. Cher Cher)", Success = false };
                 }
-                if (string.IsNullOrEmpty(person.City) || string.IsNullOrEmpty(person.StreetAddress) || string.IsNullOrEmpty(person.State) || person.State.Length != 2)
+                if (string.IsNullOrEmpty(person.City) || string.IsNullOrEmpty(person.StreetAddress) || string.IsNullOrEmpty(person.State) || person.State.Length != 2 || string.IsNullOrEmpty(person.ZipCode))
                 {
-                    return new AddPersonResponse() { Message = "Address is incorrect. Please include an address." };
+                    return new AddPersonResponse() { Message = "Address is incorrect. Please include an address.", Success = false };
+                }
+                if (person.State.Length != 2)
+                {
+                    return new AddPersonResponse() { Message = "State can only be a length of 2", Success = false };
+                }
+                if (string.IsNullOrEmpty(person.ImageUrl))
+                {
+                    return new AddPersonResponse() { Message = "Please include an image url.", Success = false };
                 }
                 //add other validation here for state, zip, and address and imgurl.
                 var entityPerson = Mapper.Map<Person>(person);
